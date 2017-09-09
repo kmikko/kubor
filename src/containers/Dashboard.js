067561984516bc9df88ee6f606dcff6501c61dba
Mikko Korhonen <mikko.korhonen@gmail.com>
2017-09-09 16:12:53 +0300

    Add some error handling for data fetching

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -16,35 +16,33 @@ class Dashboard extends React.Component {
   };
   componentDidMount() {
     fetch(
-      "http://192.168.99.100:32645/api/datasources/proxy/1/query?epoch=ms&q=SELECT+sum(%22value%22)+FROM+%22network%2Ftx_rate%22+WHERE+%22type%22+%3D+%27pod%27+AND+%22namespace_name%22+%3D~+%2Fkube-system$%2F+AND+%22pod_name%22+%3D~+%2Fheapster-8fvx3$%2F+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m)+fill(null)%0ASELECT+sum(%22value%22)+FROM+%22network%2Frx_rate%22+WHERE+%22type%22+%3D+%27pod%27+AND+%22namespace_name%22+%3D~+%2Fkube-system$%2F+AND+%22pod_name%22+%3D~+%2Fheapster-8fvx3$%2F+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m)+fill(null)"
+      "http://monitoring-grafana.kube-system/api/datasources/proxy/1/query?epoch=ms&q=SELECT%20sum%28%22value%22%29%20FROM%20%22network%2Ftx_rate%22%20WHERE%20%22type%22%20%3D%20%27pod%27%20AND%20time%20%3E%20now%28%29%20-%2030m%20GROUP%20BY%20time%281m%29%20fill%28null%29%0D%0ASELECT%20sum%28%22value%22%29%20FROM%20%22network%2Frx_rate%22%20WHERE%20%22type%22%20%3D%20%27pod%27%20AND%20time%20%3E%20now%28%29%20-%2030m%20GROUP%20BY%20time%281m%29%20fill%28null%29"
     )
       .then(response => response.json())
       .then(data => data.results)
-      .then(data =>
-        data.map(result =>
-          result.series.map(serie => ({
-            name: serie.name,
-            values: serie.values
-          }))
-        )
-      )
+      .then(data => data.map(result => result.series || []))
       .then(data => [].concat.apply([], data))
+      .then(data =>
+        data.map(serie => ({
+          name: serie.name,
+          values: serie.values
+        }))
+      )
       .then(data => this.setState({ network: data }));
 
     fetch(
-      "http://192.168.99.100:32645/api/datasources/proxy/1/query?epoch=ms&q=SELECT+sum(%22value%22)+FROM+%22cpu%2Fusage_rate%22+WHERE+%22type%22+%3D+%27node%27+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m),+%22nodename%22+fill(null)%0ASELECT+sum(%22value%22)+FROM+%22cpu%2Flimit%22+WHERE+%22type%22+%3D+%27node%27+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m),+%22nodename%22+fill(null)%0ASELECT+sum(%22value%22)+FROM+%22cpu%2Frequest%22+WHERE+%22type%22+%3D+%27node%27+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m),+%22nodename%22+fill(null)"
+      "http://monitoring-grafana.kube-system/api/datasources/proxy/1/query?epoch=ms&q=SELECT+sum(%22value%22)+FROM+%22cpu%2Fusage_rate%22+WHERE+%22type%22+%3D+%27node%27+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m),+%22nodename%22+fill(null)%0ASELECT+sum(%22value%22)+FROM+%22cpu%2Flimit%22+WHERE+%22type%22+%3D+%27node%27+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m),+%22nodename%22+fill(null)%0ASELECT+sum(%22value%22)+FROM+%22cpu%2Frequest%22+WHERE+%22type%22+%3D+%27node%27+AND+time+%3E+now()+-+30m+GROUP+BY+time(1m),+%22nodename%22+fill(null)"
     )
       .then(response => response.json())
       .then(data => data.results)
-      .then(data =>
-        data.map(result =>
-          result.series.map(serie => ({
-            name: serie.name,
-            values: serie.values
-          }))
-        )
-      )
+      .then(data => data.map(result => result.series || []))
       .then(data => [].concat.apply([], data))
+      .then(data =>
+        data.map(serie => ({
+          name: serie.name,
+          values: serie.values
+        }))
+      )
       .then(data => this.setState({ cpu: data }));
   }
 
